2a25a8e084429f3ac8b706f2c22febb274ae9931
Alex Hayton <alex.hayton@hopin.to>
2024-07-30 14:32:19 +0100

    Again

---
+++ scripts/set-windows-msvc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create a .cargo directory if it does not exist
+mkdir -p .cargo
+
+# Create a config file with the target set to x86_64-pc-windows-msvc
+echo "[build]" > .cargo/config
+echo "target = "x86_64-pc-windows-msvc" >> .cargo/config
