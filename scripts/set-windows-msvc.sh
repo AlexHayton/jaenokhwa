2b20ffe8048767fb42ae141b7aa1054aca3849ab
Alex Hayton <alex.hayton@hopin.to>
2024-07-30 14:52:03 +0100

    Add mingw64 to the windows environment

--- scripts/set-windows-msvc.sh
+++ scripts/set-windows-msvc.sh
@@ -6,3 +6,6 @@ mkdir -p .cargo
 # Create a config file with the target set to x86_64-pc-windows-msvc
 echo '[build]' > .cargo/config
 echo 'target = "x86_64-pc-windows-msvc"' >> .cargo/config
+
+# Add MINGW to the PATH
+echo "PATH=$PATH:C:\msys64\mingw64\bin" >> $GITHUB_ENV
