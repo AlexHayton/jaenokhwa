9453cd2a75e6f5689fe83835b11ac2f22f68e2be
Alex Hayton <alex.hayton@hopin.to>
2024-07-30 12:34:32 +0100

    LFG

---
+++ scripts/build-all-examples.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Change directory to the example folder
+cd examples
+
+# Loop through each project in the example folder
+for project in */; do
+    # Change directory to the project folder
+    cd "$project"
+
+    # Run cargo clippy
+    cargo build
+
+    # Change directory back to the example folder
+    cd ..
+done
